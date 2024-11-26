6f99c9ef9a2025d710ebb03853b4d67c0c86ccf5
Falola Ibrahim Ayobami <46596149+ayofalola240@users.noreply.github.com>
2024-11-26 14:46:21 +0100

    new update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
+const https = require("https");
+
+// Create an HTTPS agent with certificate verification disabled
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
 
 export const createAxiosClient = (headers = {}) => {
   return axios.create({
     baseURL: typeof window === "undefined" ? `${process.env.NEXT_PUBLIC_Backend_URL}/api/v1` : "/api/v1",
     headers,
-    withCredentials: true, // Ensure cookies are included
+    withCredentials: true,
+    httpsAgent: agent,
   });
 };
 
